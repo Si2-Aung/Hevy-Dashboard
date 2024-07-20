b0ec994e8d8a7a727c66d66a19368db5f63d739d
Si Thu <C2aung7@gmail.com>
2024-07-20 12:50:25 +0200

    Layout changed

--- home_page.py
+++ home_page.py
@@ -1,3 +1,20 @@
-def main(workout_data)
+import streamlit as st
+import pandas as pd
+import data_cleaning as dc
+import streamlit_shadcn_ui as ui
+
+
+def main(workout_data):
+    st.title("Overview")
+    cols = st.columns(3)
+    with cols[0]:
+        ui.metric_card(title="Total Workouts", content="total_workouts", key="card1")
+    with cols[1]:
+        ui.metric_card(title="Average Workout Time", content="average_duration", key="card2")
+    with cols[2]:
+        ui.metric_card(title="Longest Streak in weeks", content="longest_streak", key="card3")
     return 
     
+
+def calculate_total_workouts(workout_data):
+    return str(workout_data['start_time'].nunique())
--- main_page.py
+++ main_page.py
@@ -1,23 +1,43 @@
 import streamlit as st
+import pandas as pd
 import home_page
 from streamlit_option_menu import option_menu
+import data_cleaning as dc
+
+def main():
+    workout_data = get_csv_file()
+    if workout_data is not None:
+        dc.clean(workout_data)
+        selected_option = option_menu(
+            menu_title = None,
+            options = ["Home", "Statitstic", "Contact"],
+            icons=["house", "book", "phone"],
+            menu_icon="th-large",
+            default_index=0,
+            orientation="horizontal"
+        )
+
+        if selected_option == "Home":
+            home_page.main(workout_data)
+
+        elif selected_option == "Statitstic":
+            st.title("Welcome to Statitstic Page")
+
+        elif selected_option == "Contact":
+            st.title("Welcome to Contact Page")
 
 
-selected_option = option_menu(
-    menu_title = None,
-    options = ["Home", "Statitstic", "Contact"],
-    icons=["house", "book", "phone"],
-    menu_icon="th-large",
-    default_index=0,
-    orientation="horizontal"
-)
+def get_csv_file():
+    # Allow the user to upload a CSV file
+    csv_file = st.file_uploader(" ",type="csv",label_visibility="collapsed")
+    if csv_file is not None:
+        st.success("Data uploaded successfully")
+        workout_data = pd.read_csv(csv_file)
+    else:
+        if 'uploaded_data' in st.session_state:
+            workout_data = st.session_state['uploaded_data']
+        else:
+            workout_data = None
+    return workout_data
 
-if selected_option == "Home":
-    st.title("Welcome to Home Page")
-    home_page.main()
-
-elif selected_option == "Statitstic":
-    st.title("Welcome to Statitstic Page")
-
-elif selected_option == "Contact":
-    st.title("Welcome to Contact Page")
+main()
