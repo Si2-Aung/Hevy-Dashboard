5f94568bf3b482f828670251fdc5ff9a28b49fe2
Si Thu <C2aung7@gmail.com>
2024-07-22 01:14:22 +0200

    Average duration added

--- home_page.py
+++ home_page.py
@@ -6,15 +6,25 @@ import streamlit_shadcn_ui as ui
 
 def main(workout_data):
     st.title("Overview")
+    total_workouts = calculate_total_workouts(workout_data)
+    average_duration = calculate_average_duration(workout_data)
     cols = st.columns(3)
     with cols[0]:
-        ui.metric_card(title="Total Workouts", content="total_workouts", key="card1")
+        ui.metric_card(title="Total Workouts", content=total_workouts, key="card1")
     with cols[1]:
-        ui.metric_card(title="Average Workout Time", content="average_duration", key="card2")
+        ui.metric_card(title="Average Workout Time", content=average_duration, key="card2")
     with cols[2]:
         ui.metric_card(title="Longest Streak in weeks", content="longest_streak", key="card3")
     return 
     
-
-def calculate_total_workouts(workout_data):
+# Function to calculate total workouts
+def calculate_total_workouts(workout_data: pd.DataFrame) -> str:
     return str(workout_data['start_time'].nunique())
+
+# Function to calculate average workout duration
+def calculate_average_duration(workout_data: pd.DataFrame) -> str:
+    copied_workout_data = workout_data.copy()
+    filtered_workout_data = copied_workout_data.drop_duplicates(subset=['start_time'])
+    list_of_duration_minutes = (filtered_workout_data['end_time'] - filtered_workout_data['start_time']).dt.total_seconds() / 60
+    average_duration = list_of_duration_minutes.mean()
+    return f"{round(average_duration)} min"
--- main_page.py
+++ main_page.py
@@ -11,12 +11,10 @@ def main():
     else:
         dc.clean(workout_data)
         selected_option = option_menu(
-            menu_title = None,
-            options = ["Home", "Statistic", "Contact"],
+            menu_title=None,
+            options=["Home", "Statistic", "Contact"],
             icons=["house", "book", "phone"],
-            menu_icon="th-large",
-            default_index=0,
-            orientation="horizontal"
+            menu_icon="th-large", default_index=0, orientation="horizontal"
         )
 
         if selected_option == "Home":
@@ -30,14 +28,12 @@ def main():
 
 
 def get_csv_file():
-    workout_data = None
     if 'uploaded_data' not in st.session_state:
         csv_file = st.file_uploader(" ", type="csv", label_visibility="collapsed")
         if csv_file is not None:
             st.success("Data uploaded successfully")
-            workout_data = pd.read_csv(csv_file)
             # Store the uploaded file in the session state
-            st.session_state.uploaded_data = workout_data
+            st.session_state.uploaded_data = pd.read_csv(csv_file)
             st.rerun()
     else:
         return st.session_state.uploaded_data
@@ -46,7 +42,7 @@ def how_to_upload():
     st.title("📘 How to Use This App")
 
     st.markdown("""
-    **1.** Log in at [hevy.com](https://hevy.com) to **download your CSV file** from your browser 😃
+    **1.** Log in at [hevy.com](https://hevy.com) to **download your CSV file** 😃
 
     **2.** Navigate to **Settings** and select **'Export Data'** to download your CSV file 📥
 
