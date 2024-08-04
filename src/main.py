66be4fd07a11dacedc61fd0905d65e9eb2823823
Si Thu <C2aung7@gmail.com>
2024-08-05 01:23:03 +0200

    Revert "Theme code cleaned"
    This reverts commit 12aa6f353e015ae2f6f2ff11710803717449a68b.

--- src/main.py
+++ src/main.py
@@ -10,26 +10,25 @@ from streamlit_option_menu import option_menu
 def main():
     workout_data = get_csv_file()
     if workout_data is None:    
-        display_how_to_upload()
+        how_to_upload()
     else:
         data_cleaning.clean(workout_data)
-        selected_option = create_selection_bar()
-        themes.set_page_theme(selected_option)
-        display_selected_page(selected_option, workout_data)
+        create_selection_bar(workout_data)
     
 def get_csv_file():
     if 'uploaded_data' not in st.session_state:
         csv_file = st.file_uploader(" ", type="csv", label_visibility="collapsed")
         if csv_file is not None:
             st.success("Data uploaded successfully")
+            # Store the uploaded file in the session state
             st.session_state.uploaded_data = pd.read_csv(csv_file)
             st.rerun()
     else:
         return st.session_state.uploaded_data
-    return None
     
-def display_how_to_upload():
+def how_to_upload():
     st.title("📘 How to Use This App")
+
     st.markdown("""
     **1.** Log in at [hevy.com](https://hevy.com) to **download your CSV file** 😃
 
@@ -40,26 +39,66 @@ def display_how_to_upload():
     **4.** **Enjoy the app** 🎉
     """)
 
-def create_selection_bar():
+def create_selection_bar(workout_data):
+    if 'selected_option' not in st.session_state:
+        st.session_state.selected_option = "Home"
+
     selected_option = option_menu(
             menu_title=None,
             options=["Home", "Statistic", "Contact"],
             icons=["house", "book", "phone"],
             menu_icon="th-large", default_index=0, orientation="horizontal"
-    )
-    return selected_option
-
-def display_selected_page(selected_option, workout_data):
+        )
+    
     if selected_option == "Home":
-        st.title("Overview📝")
+        if "homereloaded" not in st.session_state:
+            st.session_state.homereloaded = False
+
+        if st.session_state.selected_option != "Home":
+            st.session_state.homereloaded = True
+            st.session_state.selected_option = "Home"
+
+        if st.session_state.homereloaded:
+            themes.set_home_page_theme()
+            st.session_state.homereloaded = False
+            st.rerun()
+
+        st.title("Overview")
         home_page.main(workout_data)
+
+
     elif selected_option == "Statistic":
+        if "statisticreloaded" not in st.session_state:
+            st.session_state.statisticreloaded = False
+
+        if st.session_state.selected_option != "Statistic":
+            st.session_state.statisticreloaded = True
+            st.session_state.selected_option = "Statistic"
+
+        if st.session_state.statisticreloaded:
+            themes.set_statistic_page_theme()
+            st.session_state.statisticreloaded = False
+            st.rerun()
+
         st.title("Welcome to Statistic Page")
         stats_page.main(workout_data)
+
+
     elif selected_option == "Contact":
+        if "contactreloaded" not in st.session_state:
+            st.session_state.contactreloaded = False
+
+        if st.session_state.selected_option != "Contact":
+            st.session_state.contactreloaded = True
+            st.session_state.selected_option = "Contact"
+
+        if st.session_state.contactreloaded:
+            themes.set_contact_page_theme()
+            st.session_state.contactreloaded = False
+            st.rerun()
+
         st.title("Welcome to Contact Page")
 
 
 
-if __name__ == "__main__":
-    main()
+main()
