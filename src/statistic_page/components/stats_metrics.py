5035d12edd54b4eeec161f3b9d1913129951e685
Si Thu <C2aung7@gmail.com>
2024-10-03 20:34:46 +0200

    Weightless_metrics added

--- src/statistic_page/components/stats_metrics.py
+++ src/statistic_page/components/stats_metrics.py
@@ -1,16 +1,31 @@
 import pandas as pd
 from src.statistic_page.components.weighted_metrics import create_weighted_metrics
-from src.statistic_page.components.weightless_metrics import create_weightless_metrics
+from src.statistic_page.components import weightless_metrics
+import streamlit as st
 
 
 
 def display_metrics(excercise_filtered_data: pd.DataFrame):
-
-    if excercise_filtered_data["weight_kg"].isna().any():
-        create_weightless_metrics(excercise_filtered_data)
-
-    else:
+    # Check for the first case (weight present)
+    if not excercise_filtered_data["weight_kg"].isna().all():
         create_weighted_metrics(excercise_filtered_data)
+    
+    # Check for the second case (both duration and distance present)
+    elif not excercise_filtered_data["duration_seconds"].isna().all() and not excercise_filtered_data["distance_km"].isna().all():
+        weightless_metrics.create_duration_distance_metrics(excercise_filtered_data)
+    
+    # Case where only duration is available, no distance
+    elif not excercise_filtered_data["duration_seconds"].isna().all() and excercise_filtered_data["distance_km"].isna().all():
+        weightless_metrics.create_duration_only_metrics(excercise_filtered_data)
 
+    # Check for the third case (reps present, no weight, duration, or distance)
+    elif not excercise_filtered_data["reps"].isna().all() and excercise_filtered_data["weight_kg"].isna().all() and excercise_filtered_data["duration_seconds"].isna().all() and excercise_filtered_data["distance_km"].isna().all():
+        weightless_metrics.create_reps_metrics(excercise_filtered_data)
 
+    # Fourth case (no weight, duration, distance, or reps)
+    else:
+        st.warning("No data available for the selected filters.")
+        return False
+
+    return True
 
--- src/statistic_page/components/weightless_metrics.py
+++ src/statistic_page/components/weightless_metrics.py
@@ -3,16 +3,52 @@ import pandas as pd
 from streamlit_extras.metric_cards import style_metric_cards
 
 
-
-def create_weightless_metrics(excercise_filtered_data: pd.DataFrame):
-    colss = st.columns(4)
+def display_metric(label1: str, value1: str, label2: str, value2: str):
+    colss = st.columns(2)
     with colss[0]:
-        st.metric(label="Total Workout done", value="weightless")
+        st.metric(label=label1, value=value1)
     with colss[1]:
-        st.metric(label="⌀ Workout Time", value="weightless")
-    with colss[2]:
-        st.metric(label="Longest Streak", value="weightless")
-    with colss[3]:
-        st.metric(label="Most trained in a week", value="weightless")
+        st.metric(label=label2, value=value2)
     style_metric_cards("#FFF", 1, "#CCCCCC", 10, "#008DFF",True)
+    return
+
+def format_duration(duration_in_sec: float) -> str:
+    duration_in_min = duration_in_sec / 60
+    if duration_in_min <= 2:
+        return f"{int(duration_in_sec)} sec"
+    else:
+        minutes = int(duration_in_sec // 60)
+        seconds = int(duration_in_sec % 60)
+        return f"{minutes}:{seconds:02d} min"
+    
+def calculate_best_time_per_km(excercise_data: pd.DataFrame):
+    running_data = excercise_data[excercise_data['exercise_title'] == 'Running']
+    if running_data.empty or running_data['distance_km'].sum() == 0:
+        return 0  
+    running_data['time_per_km'] = running_data['duration_seconds'] / running_data['distance_km']
+    return running_data['time_per_km'].min()
+
+def create_duration_distance_metrics(excercise_filtered_data: pd.DataFrame):
+    most_distance_value = str(excercise_filtered_data["distance_km"].max()) + " km"
+    best_time_per_km = calculate_best_time_per_km(excercise_filtered_data)
+    formatted_best_time_per_km = format_duration(best_time_per_km)
+
+    display_metric("Furthest distance", most_distance_value, "Best Time per km", formatted_best_time_per_km)
+    return
+
+def create_duration_only_metrics(excercise_filtered_data: pd.DataFrame):
+    longest_duration_in_sec = excercise_filtered_data["duration_seconds"].max()
+    avg_duration_in_sec = excercise_filtered_data["duration_seconds"].mean()
+   
+    longest_duration = format_duration(longest_duration_in_sec)
+    avg_duration = format_duration(avg_duration_in_sec)
+
+    display_metric("Longest duration", longest_duration, "⌀ Average duration", avg_duration)
+    return
+
+def create_reps_metrics(excercise_filtered_data: pd.DataFrame):
+    most_reps = excercise_filtered_data["reps"].max()
+    avg_reps = excercise_filtered_data["reps"].mean()
+
+    display_metric("Most reps", most_reps, "⌀ Average reps", avg_reps)
     return
